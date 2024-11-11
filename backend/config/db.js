0447be064754ac22863cc3478f87bbd00276976d
Kicrops <spiccininizeida@gmail.com>
2024-11-10 21:27:38 -0300

    Added db folder

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 let db;
 
 const connectDB = () => {
-    db = new sqlite3.Database('./database.db', (err) => {
+    db = new sqlite3.Database('./db/database.db', (err) => {
         if (err) {
             console.error('Error connecting to database:', err.message);
             process.exit(1);
